0f015be018d7681589d7d7a17aa99798d503c8b3
DavidBen200 <davidbenn200@gmail.com>
2023-07-18 22:04:01 +0100

    Updated comment

--- components/utils/index.js
+++ components/utils/index.js
@@ -6,7 +6,7 @@ export const requiredProp = (type) => ({
     // New comment
     // another change
     // Chidi made this one!
-    // this one is a wrong stuff! ? Why did u do this?
+    // this one is a wrong stuff! ? Why did u do this? -- I did it for blah
 })
 
 export const requiredStringProp = requiredProp(String)
